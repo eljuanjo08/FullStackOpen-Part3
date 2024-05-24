19d1ca8936d5f6ad0d084e51b08b27426c46f06c
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-05-24 15:55:56 -0600

    Ejercicio 3.2

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hola mundo</h1>')
-})
+const date = Date.now()
+const today = new Date(date)
 
-app.get('/api/persons/', (request, response) => {
-  response.send(persons)
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hola mundo</h1>");
+});
+
+app.get("/api/persons/", (request, response) => {
+  response.send(persons);
+});
+
+app.get("/info", (request, response) => {
+  console.log(date)
+  response.send(
+    `<p>
+      Phonebook has info for ${persons.length} people<br/>
+      ${today}
+    </p>`);
+});
 
 const PORT = 3001;
-app.listen(PORT, ()=> {
-  console.log(`server running in port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running in port ${PORT}`);
+});
