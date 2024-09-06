21bacfeedc07b16a55e8e01a4f172a7ee683039c
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-09-05 20:52:20 -0600

    Ejercicio 3.7_3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
 app.use(express.json());
+app.use(morgan("tiny"))
+
+const middleware = (req, res, next) => {
+  console.log(req.body);
+  next();
+}
+
+app.use(middleware)
 
 let persons = [
   {
